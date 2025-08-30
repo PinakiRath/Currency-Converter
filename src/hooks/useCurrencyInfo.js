3100c2a643c2125a5349fb44bc409dc453e24dee
Pinaki <pinakirath99@gmail.com>
2025-08-30 20:48:55 +0530

    Update: fixed currency hook, improved InputBox, updated App, added bg image

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -2,15 +2,20 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
+
   useEffect(() => {
     fetch(
       `https://v6.exchangerate-api.com/v6/f2c12f4ece91b95a7e9979ae/latest/${currency}`
     )
       .then((res) => res.json())
-      .then((res) => setData(res[currency]));
-    console.log(data);
+      .then((res) => {
+        if (res && res.conversion_rates) {
+          setData(res.conversion_rates);
+        }
+      })
+      .catch((err) => console.error("Error fetching currency data:", err));
   }, [currency]);
-  console.log(data);
+
   return data;
 }
 
